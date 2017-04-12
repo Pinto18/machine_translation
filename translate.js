d8039974b31a3a3fcbbae5482f17ac47c12de661
Pinto18 <pinto18.nd@gmail.com>
2017-04-11 18:31:42 -0700

    Created html and javascript files

---
+++ translate.js
@@ -0,0 +1,9 @@
+function translateSentence() {
+  //Load Sentence into Variable
+  var mySentence = document.getElementById("mySentence").value;
+  var myDictionary = document.getElementById("myDictionary").value;
+	mySentence = mySentence.split(" ");
+  //myDictionary = myDictionary.split(",").split(":").join(" ").join(" ").split(" ");
+  //sentence = sentence.map((val)=>myDictionary[myDictionary.indexOf(val)+1]);
+	
+  
