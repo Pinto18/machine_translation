51ea3e0fc56faecd473568c2ab46e8a8e8dd2932
VelardeR <robert_Vel@cox.net>
2017-04-17 19:26:28 -0700

    Update translate.cpp
    I have added the function implementations for Translating and Formatting. All of the functions utilize string pointer to simplify the main program, but I can easily change it to return a string if we want. An interesting thing to note is: because translate has no return call, any word will not be translated if it does not have a translation. This makes it work well for proper nouns such as names. If you put "Nick is smart" it cannot translate "Nick" and will put that into the translation and write "nick es inteligente". Just a cool thing to note.

--- translate.cpp
+++ translate.cpp
@@ -8,28 +8,72 @@
 #include<string>
 using namespace std;
 
+#define A 65			// ASCII code for 'A'
+#define Z 90			// ASCII code for 'Z'
+#define a 97			// ASCII code for 'a'
+#define z 122			// ASCII code for 'z'
+#define LOWERCASE 32	// ASCII conversion for uppercase to lowercase
+
+void translateWord(string &word);
+void onlyAlphabetical(string &input);
+void toLowercase(string &input);
+void formatString(string &input);
+
 int main()
 {
-	ifstream spanish_file;
-	string input, line, english_token, spanish_token;
-	
-	spanish_file.open("spanish.txt");
-	cout << "Please enter an english sentence: ";
+	ofstream output("output.txt");	// File to hold translated sentence
+	string input, line;
+
+	cout << "Enter an English sentence: ";
 	getline(cin, input);
-	istringstream iss(input);	//used to split user input into separate words
-	string word;			//string that represents individual words in user input
-	while(iss >> word)		//keep looping until you reach the end of a user's sentence
+	output << "English: " << input << endl;
+	formatString(input);
+	output << "Formatted: " << input << endl << "Spanish: ";
+	istringstream iss(input);	// Splits sentence into seperate words
+	string word;				// Used to hold an individual word
+	while(iss >> word)			//Loop through each word in the sentence
 	{
-		while(getline(spanish_file, line))
-		{
-			english_token = line.substr(0, line.find(":"));
-			spanish_token = line.substr(line.find(":") + 1, line.find("\n"));
-			if(word.compare(english_token) == 0)
-				cout << spanish_token << " ";
-		}
-		spanish_file.clear();
-		spanish_file.seekg(0, ios::beg);
+		translateWord(word);
+		output << word << " ";
+		cout << word << " ";
 	}
-	cout << endl;
-	spanish_file.close();
+	output.close();
 }
+
+void translateWord(string &word) {
+	ifstream dictionary("translation.txt");		// English to Spanish text file
+	char delimeter = ':';
+	string line, english_token, spanish_token;
+
+	while(getline(dictionary, line)) {
+		english_token = line.substr(0, line.find(delimeter));
+		spanish_token = line.substr(line.find(delimeter) + 1, line.find("\n"));
+		if(word.compare(english_token) == 0) {	// Word is found in dictionary
+			dictionary.close();
+			word = spanish_token;	// Replace with the Spanish translation
+			break;
+		}
+	}
+
+}
+
+void formatString(string &input) {
+	onlyAlphabetical(input);
+	toLowercase(input);
+}
+
+void onlyAlphabetical(string &input) {
+	for(string::size_type i = 0; i < input.size(); i++) {	// Iterate each char
+		if ( !(((input[i] >= A) && (input[i] <= Z)) ||		// Uppercase check
+			   ((input[i] >= a) && (input[i] <= z)))) {		// Lowercase check
+			input[i] = ' ';					// Replace character with a space
+		}
+	}
+}
+
+void toLowercase(string &input) {
+	for(string::size_type i = 0; i < input.size(); i++) {	// Iterate each char
+		if ((input[i] >= A) && (input[i] <= Z)) {		// Uppercase check
+			input[i] += LOWERCASE;						// Convert to lowercase
+		}
+	}}
